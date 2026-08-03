c868466dc071de794aa711c0509555cc464da1b9
samreen0904 <samreenshah2008@gmail.com>
2026-08-03 11:56:40 -0700

    assigment donneee

---
+++ chapter9-11/app.js
@@ -0,0 +1,202 @@
+// question # 1 start
+
+
+
+
+// var city = prompt("Enter Yiur city name")
+// if (city === "karachi"){
+//     alert("Welcome to city of light")
+// }
+
+// question # 1 end
+
+
+
+
+// question # 2 start
+// var gender = prompt("Enter your gender")
+// if (gender === "male"){
+//     alert("Good Morning Sir")
+// }else if (gender === "female") {
+//     alert("Good Morning Ma'am");
+// }
+// question # 2 end
+
+
+
+
+// question # 3 start
+
+// var light = prompt("Enter your traffic color")
+// if (light === "red")
+//     alert("Must Stop")
+// else if (light === "yellow"){
+//     alert("Ready to move")
+// }
+// else if (light === "green"){
+//     alert("Move now")
+// }
+
+
+
+
+// question # 3 end
+
+
+
+// question # 4 start
+
+// var fuel = prompt("Enter remaining fuel in litres:");
+
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// }
+
+
+// question # 4 end
+
+// question # 5 start
+
+// a.true
+
+// b.false
+
+// c condition 1.false
+// c condition 2.true
+// c condition 3.false
+// c condition 4.true
+
+// d. The cost equals
+
+// e 1.true
+// e 2.false
+
+
+//f. true
+
+
+
+
+
+
+
+// question # 5 end
+
+
+
+// question # 6 satrt
+
+// var subject1 = +prompt("Enter marks of Subject 1:");
+// var subject2 = +prompt("Enter marks of Subject 2:");
+// var subject3 = +prompt("Enter marks of Subject 3:");
+// var totalMarks = +prompt("Enter Total Marks:");
+
+// var obtainedMarks = subject1 + subject2 + subject3;
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// var grade;
+// var remarks;
+
+// if (percentage >= 80) {
+//     grade = "A-One";
+//     remarks = "Excellent";
+// } else if (percentage >= 70) {
+//     grade = "A";
+//     remarks = "Good";
+// } else if (percentage >= 60) {
+//     grade = "B";
+//     remarks = "You need to improve";
+// } else {
+//     grade = "Fail";
+//     remarks = "Sorry";
+// }
+
+// document.write("<h2>Mark Sheet</h2>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + obtainedMarks + "<br>");
+// document.write("Percentage: " + percentage + "%<br>");
+// document.write("Grade: " + grade + "<br>");
+// document.write("Remarks: " + remarks);
+
+
+
+// question # 6 end
+
+
+// question # 7 start
+
+// var secretNumber = 7;
+// var guess = +prompt("Guess the secret number (1 to 10):");
+
+// if (guess === secretNumber) {
+//     alert("Bingo! Correct answer");
+// } else if (guess + 1 === secretNumber) {
+//     alert("Close enough to the correct answer");
+// }
+// question # 7 end
+
+
+
+
+// question # 8 start
+
+// var divedNum = +prompt("Give any number that is divisible by 3.")
+// if (divedNum % 3 === 0){
+//     alert("The number is divisible by 3")
+// }
+
+// question # 8 end
+
+
+
+
+
+// question # 9 start
+
+// var number = +prompt("Enter a number:");
+
+// if (number % 2 === 0) {
+//     alert("The number is Even");
+// } else {
+//     alert("The number is Odd");
+// }
+// question # 9 end
+
+
+// question # 10 start
+
+// var temperature = +prompt("Enter the temperature:");
+
+// if (temperature > 40) {
+//     alert("It is too hot outside.");
+// } else if (temperature > 30) {
+//     alert("The Weather today is Normal.");
+// } else if (temperature > 20) {
+//     alert("Today's Weather is cool.");
+// } else if (temperature > 10) {
+//     alert("OMG! Today's weather is so Cool.");
+// }
+// question # 10 end
+
+
+// question # 11 start
+
+// var num1 = +prompt("Enter first number:");
+// var num2 = +prompt("Enter second number:");
+// var operation = prompt("Enter operation (+, -, *, /, %)");
+
+// if (operation === "+") {
+//     alert(num1 + num2);
+// } else if (operation === "-") {
+//     alert(num1 - num2);
+// } else if (operation === "*") {
+//     alert(num1 * num2);
+// } else if (operation === "/") {
+//     alert(num1 / num2);
+// } else if (operation === "%") {
+//     alert(num1 % num2);
+// }
+// question # 11 end
+
+ 
+
