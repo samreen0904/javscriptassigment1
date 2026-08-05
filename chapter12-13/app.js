9b07787ebc1b58032bef2209f804c9066878fcba
samreen0904 <samreenshah2008@gmail.com>
2026-08-05 03:55:10 -0700

    12-13 assigment done

---
+++ chapter12-13/app.js
@@ -0,0 +1,122 @@
+// question # 2 start
+// var firstNum = +prompt("Enter your first number")
+// var secondNum = +prompt("Enter your second number")
+
+
+// if (firstNum > secondNum){
+//     alert("first number is larger")
+
+// }
+// else if (firstNum < secondNum){
+//     alert("secound number is larger")
+
+// }
+// else {
+
+//     alert("both number are equal")
+// }
+
+// question # 2 end
+
+
+
+// question # 3 start
+// var num1 = +prompt("Enter your number")
+  
+
+// if (num1 > 0){
+//     alert("this number is positive")
+
+// }
+// else if (num1 < 0){
+//     alert("this number is negative")
+
+// }
+// else {
+
+//     alert("this number is zero")
+// }
+
+
+
+// question # 3 end
+
+
+
+// question # 4 start
+
+// var cha = prompt("Enter a character")
+
+
+// if(cha == "a" || cha == "e" || cha == "i" || cha == "o" || cha == "u" ||
+//     cha == "A" || cha == "E" || cha == "I" || cha == "O" || cha == "U")
+//     {
+//     alert("true")
+
+// }
+// else{
+//     alert("false")
+// }
+// question # 4 end
+
+
+
+// question # 5 start
+// var correctPassword = "samreen0904"
+// var userpassword = prompt("Enter you password")
+
+// if (userpassword == ""){
+//     alert( "please Enter your password")
+
+// }
+// else if (userpassword == correctPassword){
+//     alert("Correct! The password you entered matches the original password")
+
+// }
+// else{
+//     alert("incorrect password")
+// }
+
+// question # 5 end
+
+
+
+// question # 6 start
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+
+// alert(greeting);
+
+
+
+
+// question # 6 end
+
+
+// question # 7 start
+// var time = +prompt("Enter time in 24-hour format (e.g. 1900):");
+
+// if (time >= 1 && time < 1200) {
+//     alert("Good Morning!");
+// }
+// else if (time >= 1200 && time < 1700) {
+//     alert("Good Afternoon!");
+// }
+// else if (time >= 1700 && time < 2100) {
+//     alert("Good Evening!");
+// }
+// else if (time >= 2100 && time <= 2359) {
+//     alert("Good Night!");
+// }
+// else {
+//     alert("Invalid Time");
+// }
+// question # 7 end
+
+
