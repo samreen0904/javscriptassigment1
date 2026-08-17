43c5b8a2419a61f0a6c0a4ebca7bde3fe867972c
samreen0904 <samreenshah2008@gmail.com>
2026-08-17 03:34:20 -0700

    assigment donee

---
+++ chapter13-15/app.js
@@ -0,0 +1,292 @@
+// question # 1 start
+// var studentName = newArray();
+
+// question # 1 end
+
+
+
+
+// question # 2 start
+// var studentName = [];
+// question # 2 end
+
+
+
+
+
+
+// question # 3 start
+// var colorName = ["Red","blue","purpule","pink"]
+// question # 3 end
+
+
+
+
+
+
+// question # 4 start
+
+// var multiNum = [10,20,30,40]
+// question # 4 end
+
+
+
+
+// question # 5 start
+// var value = [true,false]
+// question # 5 end
+
+
+
+
+// question # 6 start
+// var multiValue = ["samreen",16,true,"apple",20,false]
+// question # 6 end
+
+
+
+// question # 7 start
+
+
+// let qualifications = ["SSC   "  , "HSC    ", "BCS     ", "BS    ", "BCOM      ", "MS     ", "M. Phil.        ", "PhD     "];
+
+// document.write(qualifications);
+// question # 7 end
+
+
+
+
+
+// question # 8 start
+// var studentName = ["Sabeen","Hania","Maria"]
+// var score = [480,400,360]
+// var percentage1 = (score[0] / 500) *100
+// var percentage2 = (score[1] / 500) *100
+// var percentage3 = (score[2] / 500) *100
+
+
+// document.write("score   of " + studentName[0] + "  is  " + score[0]  +   ".     percentage:"   + percentage1 + "%<br>"  )
+// document.write("score   of " + studentName[1] + "  is  " + score[1]  +   ".     percentage:"   + percentage2 +  "%<br>" )
+// document.write("score   of " + studentName[2] + "  is  " + score[2]  +   ".     percentage:"   + percentage3 +   "%<br>" )
+// question # 8 end
+
+
+
+
+// question # 9 start
+// var colors = ["Red", "Green", "Blue", "Yellow"];
+
+// part a start
+
+// var addColor = prompt("koi color add karo  start me");
+// colors.unshift(addColor);
+// document.write(colors);
+
+
+// part a end
+
+
+
+// part b start
+// var addEnd = prompt("koi color add karo end me")
+// colors.push(addEnd);
+// document.write(colors);
+
+// part b end
+
+
+
+// part c start
+
+// colors.unshift("pink","black");
+// document.write(colors);
+// part c end
+
+
+
+// part d start
+// colors.shift();
+// document.write(colors);
+// part d end
+
+
+ // part e start
+// colors.pop()
+// document.write(colors)
+// part e end
+
+
+
+
+// part f start
+// var indexNum = prompt("Enter a index number")
+// var addcolor = prompt("Enter a color")
+
+
+// colors.splice(indexNum ,0, addcolor );
+// document.write(colors)
+// part f end
+
+
+
+
+// part g start
+
+
+
+// var colors = ["Red", "Green", "Blue", "Yellow", "Pink"];
+
+// var index = prompt("At which index do you want to delete color(s)?");
+// var number = prompt("How many colors do you want to delete?");
+
+// colors.splice(index, number);
+
+// document.write(colors);
+// part g end
+
+
+
+
+
+// question # 9 end
+
+
+
+// question # 10 start
+// var num = [320,230,480,120];
+// document.write("Sourse   of    Student   :      " + num)
+// document.write("<br>")  
+// num.sort()
+// document.write("Ordered Source of Student    :   " + num) 
+
+
+// question # 10 end
+
+
+
+
+
+
+// question # 11 start
+// var cityList = ["karachi" , "Lahore","Islamabad","Quetta","Peshawar"];
+// document.write("<h1>Cities List:</h1>")
+// document.write(cityList)
+// document.write("<br>")
+// var selectCity = cityList.slice(2,4)
+// document.write("<h1>Selected cities List</h1>")
+
+// document.write(selectCity)
+// question # 11 end
+
+
+
+
+
+
+
+
+
+// question # 12 start
+// var arr = ["This  " ,  "  is  " , "  my  " ,  "  cat  "]
+// document.write("Array :  ")
+// document.write("<br>")
+// document.write(arr)
+// document.write("<br>")
+// document.write("String:")
+// document.write("<br>")
+// var text = arr.join("");
+// document.write(text)
+
+
+// question # 12 end
+
+
+
+
+
+
+
+
+// question # 13 start
+// var arr = [];
+
+// arr.push("keyboard");
+// arr.push("mouse");
+// arr.push("printer");
+// arr.push("monitor");
+
+// document.write(arr);
+
+// document.write("<br>");
+
+// document.write(arr.shift());
+// document.write("<br>")
+
+// document.write(arr.shift());
+// document.write("<br>")
+// document.write(arr.shift());
+// document.write("<br>")
+// document.write(arr.shift());
+// document.write("<br>")
+
+
+
+// question # 13 end
+
+
+
+
+
+
+
+
+
+
+// question # 14 start
+// var arr = [];
+
+// arr.push("keyboard");
+// arr.push("mouse");
+// arr.push("printer");
+// arr.push("monitor")
+
+// document.write(arr);
+
+// document.write("<br>");
+
+// document.write(arr.pop());
+// document.write("<br>")
+// document.write(arr.pop());
+// document.write("<br>")
+// document.write(arr.pop());
+// document.write("<br>")
+// document.write(arr.pop());
+
+
+
+// question # 14 end
+
+
+
+
+
+
+
+
+
+
+
+// question # 15 start
+// var phones = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+// document.write("<select>")
+
+// document.write("<option>"  +  "</option>")
+// document.write("<option>" + phones[0] +  "</option>")
+// document.write("<option>" + phones[1] +  "</option>")
+// document.write("<option>" + phones[2] +  "</option>")
+// document.write("<option>" + phones[3] +  "</option>")
+// document.write("<option>" + phones[4] +  "</option>")
+
+// document.write("</select>")
+
+// question # 15 end
