65c6a93e682d6898ebc0bc2c0c4e05ff7448eabc
samreen0904 <samreenshah2008@gmail.com>
2026-08-27 11:44:30 +0500

    assigment 21-25 done

---
+++ chapter21-25/app.js
@@ -0,0 +1,39 @@
+// question # 1 start
+// var firstName = prompt("enter   your first name")
+// var lastName = prompt("enter   your last name")
+
+// var fullName = firstName + " " +  lastName
+// document.write(fullName)
+
+// question # 1 end
+
+
+// question # 2 start
+// var mobileModel = prompt("Enter your favorite mobile phone model:");
+
+// document.write("My favorite phone is: " + mobileModel + "<br>");
+// document.write("Length of user input: " + mobileModel.length);
+
+// question # 2 end
+
+
+
+// question # 3 start
+// var city = "pakistan"
+// var citynew = city.indexOf("n")
+
+// document.write("  string  : " + city)
+// document.write("</br>")
+// document.write("Index Of 'n'    :   " +    citynew)
+
+
+
+
+// question # 3 end
+
+
+
+// question # 4 start
+
+
+// question # 4 end
